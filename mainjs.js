488bddd6b8915c9159c3838d86ee3f9bc1c12e53
Vsevolod Skvortsov <ss28848931@gmail.com>
2016-08-07 14:52:18 +0300

    second commit

--- mainjs.js
+++ mainjs.js
@@ -1,39 +1,34 @@
 var time = 0; 
 var running = 0; 
-
-function startPause() {
+var mins = 0;
+function start() {
 	if (running == 0) {
 		running = 1;
 		increment();
-		document.getElementById("startPause").innerHTML = "Pause";
+		document.getElementById("start").innerHTML = "Pause";
 	} else{
 		running = 0;
-		document.getElementById("startPause").innerHTML = "Resume";
+		document.getElementById("start").innerHTML = "Resume";
 	}
 }
 
 function reset() {
 	running = 0;
 	time = 0;
-	document.getElementById("startPause").innerHTML = "Start";
-	document.getElementById("output").innerHTML = "00:00";
+	document.getElementById("start").innerHTML = "Start";
+	document.getElementById("timer").innerHTML = "0:0";
 }
 
 function increment() {
 	if (running == 1){
 		setTimeout(function(){
 			time++;
-			var mins = Math.floor(time/10/60);
-			var secs = Math.floor(time/10);
-			var tenths = time%10;
-
-			if(mins < 10){
-				mins = "0"+mins;
+			var secs = Math.floor(time/10); 
+			if (time > 590){
+				time = time - 590;
+				mins++;
 			}
-			if(secs < 10){
-				secs = "0"+secs;
-			}
-			document.getElementById("output").innerHTML = mins + ":" + secs;
+			document.getElementById("timer").innerHTML = mins + ":" + secs;
 			increment();
 		}, 100);
 	}  
